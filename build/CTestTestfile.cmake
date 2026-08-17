6ddca5e53c350533148e8b49f739b4ab4e05bd2c
cicuvc <cicuvc.lectine@gmail.com>
2026-08-17 17:06:28 +0800

    chore: drop stray repo-root build/ tree from git (285M cmake output); ignore build/

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cicuvc/cs/projects/nvidia-sass-document/semu
-# Build directory: /home/cicuvc/cs/projects/nvidia-sass-document/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("cli")
-subdirs("tests")
